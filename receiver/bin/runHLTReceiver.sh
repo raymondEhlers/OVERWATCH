3c38da1c83587687607a882c59c535435a05c3a6
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2016-05-05 16:48:31 -0400

    Continue hlt script update

--- receiver/bin/runHLTReceiver.sh
+++ receiver/bin/runHLTReceiver.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# Script to connect to HLT proxy
+# Script to setup proxy
 
 # Handle the user not passing the proper options
-if [[ "$#" -ne 3 ]];
+if [[ "$#" -ne 4 ]];
 then
-    echo "Did not define normal number of arguments. Gave $#, but expected 3"
+    echo "Did not define normal number of arguments. Gave $#, but expected 4"
     echo "Continuing using some defualt values."
 fi
 
@@ -21,47 +21,57 @@ PATH="$currentLocation:$PATH"
 echo "Loading configruation variables!"
 source "$currentLocation/hltReceiverConfiguration.sh"
 
-echo "Loading alice software from ${aliceSoftwarePath}"
+echo "Loading alice software from \"${aliceSoftwarePath}\""
 . ${aliceSoftwarePath}/alice-env.sh -n 1 -q
 
-exit 0
+echo "Moving to data directory: \"${dataLocation}\""
+cd "${dataLocation}"
 
 internalPort=${1:-40321}
 externalPort=${2:-60321}
 receiverType=${3:-"EMC"}
+useSSHTunnel=${4:-true}
 
 if [[ "$receiverType" == "HLT" ]];
 then
     monitorPort=25006
-    additionalOptions="-otherHists"
 else
     monitorPort=25005
-    additionalOptions=""
 fi
+
+additionalOptions=""
 
 echo -e "\nSettings:"
 echo "internalPort: $internalPort"
 echo "externalPort: $externalPort"
 echo "receiverType: $receiverType"
-echo "monitorPort: $monitorPort"
+echo "SSH Monitor Port: $monitorPort"
+echo "Use SSH tunnel: $useSSHTunnel"
 echo "additionalOptions: $additionalOptions"
 
-# Find process
-#sshProcesses=$(pgrep -f "$internalPort:localhost")
-sshProcesses=$(pgrep -f "autossh -M $monitorPort")
-echo "sshProcesses: $sshProcesses"
+# TEMP
+exit 0
 
-# Determine if ssh tunnel is needed.
-# autossh should ensure that the connection never dies.
-if [[ -z "$sshProcesses" ]];
+if [[ "${useSSHTunnel}" == true ]];
 then
-    echo "Did not find necessary $receiverType autossh tunnel. Starting a new one!"
-    autossh -M $monitorPort -f -N -L $internalPort:localhost:$externalPort emcalguest@lbnl5core.cern.ch
+    # Find SSH process
+    sshProcesses=$(pgrep -f "autossh -M $monitorPort")
+    echo "autossh PID: $sshProcesses"
+
+    # Determine if ssh tunnel is needed.
+    # autossh should ensure that the connection never dies.
+    if [[ -z "$sshProcesses" ]];
+    then
+        echo "Did not find necessary $receiverType autossh tunnel. Starting a new one!"
+        autossh -M $monitorPort -f -N -L $internalPort:localhost:$externalPort emcalguest@lbnl5core.cern.ch
+    else
+        echo "$receiverType autossh tunnel already found with PID $sshProcesses. Not starting another one."
+    fi
 else
-    echo "$receiverType autossh tunnel already found with PID $sshProcesses. Not starting another one."
+    echo "Not using an SSH tunnel!"
 fi
 
 echo "$PWD"
 
 # select="" ensures that we get all histograms!
-ZMQhistViewer in="REQ>tcp://localhost:$internalPort" Verbose sleep=60 timeout=1000 -drawoptions="colzlogy" select="" ${additionalOptions}
+zmqReceive --in="REQ>tcp://localhost:$internalPort" --verbose=1 --sleep=60 --timeout=100 --select="" --subsystem="${receiverType}" ${additionalOptions}
