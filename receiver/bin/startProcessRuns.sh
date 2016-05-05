19da072a876256902094a8fac83c183ba24d9f45
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2016-05-05 00:58:31 -0400

    Updated processing script to allow locking out

--- receiver/bin/startProcessRuns.sh
+++ receiver/bin/startProcessRuns.sh
@@ -3,6 +3,14 @@
 # To use with crontab, use line:
 # * * * * * /home/rehlers/yaleDev/EMCalDev/qa/HLT/processFiles/startProcessRootFile.sh > /home/rehlers/yaleDev/EMCalDev/qa/HLT/processFiles/processRootFile.log 2>&1
 
+# Only process if we are not locked out.
+# Locking out allows changes to processing (for example, reprocessing) without having to disable the crontab
+if [[ -e "lockOut" ]];
+then
+    echo "ATTENTION: Processing locked out! Remove \"lockOut\" file to resume processing with this script!"
+    echo "Exiting..."
+    exit 0
+fi
 
 #echo "$PWD"
 
@@ -12,8 +20,8 @@
 # http://stackoverflow.com/a/246128 
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #echo "$DIR"
-# Need to get out of the bin/ directory
-cd $DIR/..
+# Need to get out of the receiver/bin/ directory
+cd $DIR/../..
 
 #echo "$PWD"
 
