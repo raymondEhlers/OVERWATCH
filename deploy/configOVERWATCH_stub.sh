621f2334334ae9dacc5f60b3be320a47947ed82c
jmulliga <james.mulligan@yale.edu>
2016-09-02 18:12:31 -0400

    Combine receiver scripts into initOVERWATCH.sh, and condense a single config file and a separate set of shared functions.
    There is now a single init script initOVERWATCH.sh that (1) if run on a processing
    machine: starts the receivers (if necessary) and runs processing, or (2) if run on a server
    machine: starts the wsgi server. This includes automatic detection if the receivers
    are running or if they need to be started, as well as a config option to force a restart
    of all receivers. This init script is configured by a single config file configOVERWATCH.sh,
    and utilizes a set of shared functions sharedFunctions.sh.

---
+++ deploy/configOVERWATCH_stub.sh
@@ -0,0 +1,77 @@
+# Configuration file for the HLT receiver
+
+# Option to kill any existing receivers, and start new receivers
+forceRestart=false
+
+# If alibuild is used, must specify name
+alibuildName="latest-ali-master"
+
+# Using SSH tunnel
+useSSHTunnel=false
+
+# Values are paired together
+subsystems=( "EMC" "TPC" "HLT" )
+# These are the port values that we pass to the receiver
+internalReceiverPorts=( 11 12 13)
+# These are the port values at the other host
+externalReceiverPorts=( 21 22 23)
+
+# Ensure the sizes of the variables above are the same!
+# Otherwise, throw error and exti!
+if [[ ${#subsystems[@]} -ne ${#internalReceiverPorts[@]} || ${#subsystems[@]} -ne ${#externalReceiverPorts[@]} ]];
+then
+    echo -e "\n\nERROR: Mismatch in configuration array sizes! Exiting!\n"
+    exit -1
+fi
+
+# Determine config variables, based on which machine we are running on
+
+projectPath=""
+softwarePath=""
+virtualEnvPath=""
+location=""
+buildType=""
+role=""
+
+if [[ $HOSTNAME == *"pdsf"* || $HOSTNAME == *"sgn"* ]];
+then
+  # Define necessary variables
+  projectPath="/project/projectdirs/alice/aliprodweb/overwatch"
+  softwarePath="/project/projectdirs/alice/aliprodweb/ROOT/"
+  virtualEnvPath="/project/projectdirs/alice/aliprodweb/virtualenv/python_2_7_11/bin/activate"
+  location="PDSF"
+  buildType="root"
+  role="server"
+
+  # Additional settings
+  # Sets the library path for both python and libffi
+  export LD_LIBRARY_PATH="/project/projectdirs/alice/aliprodweb/python_bin/v2.7.11/lib:/project/projectdirs/alice/aliprodweb/essentials/install/lib64:$LD_LIBRARY_PATH"
+elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"yale"* ]];
+then
+  # Define necessary variables
+  projectPath="/opt/www/aliceoverwatch"
+  softwarePath="/opt/aliceSW/root/alice_v5-34-30/inst/"
+  virtualEnvPath="/opt/www/aliceoverwatch/.env/bin/activate"
+  location="Yale"
+  buildType="root"
+  role="server"
+
+  # Additional settings
+  # None!
+elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"cern"* ]];
+then
+  # Define necessary variables
+  projectPath="/home/emcal/overwatch"
+  softwarePath="/home/emcal/alice/sw"
+  virtualEnvPath="/home/emcal/overwatch/.env/bin/activate"
+  location="overwatchCERN"
+  buildType="aliBuild"
+  role="processing"
+
+  # Additional settings
+  # None!
+else
+  echoErrorEscaped "Cannot run on hostname $HOSTNAME. You need to define the necessary variables."
+  safeExit 1
+  return $?
+fi
--- deploy/initOVERWATCH.sh
+++ deploy/initOVERWATCH.sh
@@ -1,119 +1,58 @@
 #!/usr/bin/env bash
 
-# Exit safely regardless of using ./ or source.
-# Requires "return $?" after the call to the function to complete the process
-# The return after the safeExit() call is required because return in safeExit() gets used by the function!!
-safeExit() {
-    if [[ -z "$1" ]];
-    then
-        returnCode=1
-    else
-        returnCode="$1"
-    fi
+# This script loads the OVERWATCH environment, and either (1) if run on a processing
+# machine: starts the receivers (if necessary) and runs processing, or (2) if run on a server
+# machine: starts the wsgi server.
+# If the script is sourced rather than executed, only the environment is loaded.
 
-    if [[ "$sourcedScript" == true ]];
-    then
-        echo "return"
-        return "$returnCode"
-    else
-        echo "exit"
-        exit "$returnCode"
-    fi
-}
+# Script exits if a var is unset
+#set -o nounset
+# Script exists if an statement returns a non-zero value
+set -o errexit
+
+# Determine current location of file
+# From: http://stackoverflow.com/a/246128
+currentLocation="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Move the directory of the script to ensure that the other files can be found
+cd "$currentLocation"
+
+# Load configuration and shared functions
+if [[ ! -e "configOVERWATCH.sh" ]];
+then
+  echoInfoError "Must create configOVERWATCH.sh!!"
+fi
+  source "configOVERWATCH.sh"
+
+source "sharedFunctions.sh"
+
+echoInfoEscaped "Running `basename "$0"` at $(date) in \"$PWD\""
 
 # Determine if the script was run or sourced.
 # See: https://stackoverflow.com/a/2684300
 sourcedScript=false
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]];
 then
-    echo "INFO: Script is being sourced!"
+    echoInfoEscaped "Script is being sourced!"
     sourcedScript=true
 fi
 
+# Determine if called from systemd
 calledFromSystemd=false
-if [[ -n "$1" && ("$1" == "systemd" || "$1" == "upstart") ]];
+if [[ -n "${1:-}" && ("$1" == "systemd" || "$1" == "upstart") ]];
 then
-    echo "INFO: Script was called by $1!"
+    echoInfoEscaped "Script was called by $1!"
     calledFromSystemd=true
 fi
 
-# Determine variables
-if [[ $HOSTNAME == *"pdsf"* || $HOSTNAME == *"sgn"* ]];
-then
-    # Define necessary variables
-    projectPath="/project/projectdirs/alice/aliprodweb/overwatch"
-    softwarePath="/project/projectdirs/alice/aliprodweb/ROOT/"
-    virtualEnvPath="/project/projectdirs/alice/aliprodweb/virtualenv/python_2_7_11/bin/activate"
-    location="PDSF"
-    buildType="root"
-    role="server"
-
-    # Additional settings
-    # Sets the library path for both python and libffi
-    export LD_LIBRARY_PATH="/project/projectdirs/alice/aliprodweb/python_bin/v2.7.11/lib:/project/projectdirs/alice/aliprodweb/essentials/install/lib64:$LD_LIBRARY_PATH"
-elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"yale"* ]];
-then
-    # Define necessary variables
-    projectPath="/opt/www/aliceoverwatch"
-    softwarePath="/opt/aliceSW/root/alice_v5-34-30/inst/"
-    virtualEnvPath="/opt/www/aliceoverwatch/.env/bin/activate"
-    location="Yale"
-    buildType="root"
-    role="server"
-
-    # Additional settings
-    # None!
-elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"cern"* ]];
-then
-    # Define necessary variables
-    projectPath="/home/emcal/overwatch"
-    virtualEnvPath="/home/emcal/overwatch/.env/bin/activate"
-    location="overwatchCERN"
-    buildType="aliBuild"
-    role="processing"
-    softwarePath="/home/emcal/alice/sw"
-
-    # Additional settings
-    # None!
-elif [[ "$HOSTNAME" == "lbnl5core" ]];
-then
-    # Define necessary variables
-    projectPath="/home/emcalguest/overwatch"
-    virtualEnvPath="/home/emcalguest/overwatch/.env/bin/activate"
-    location="lbnl5"
-    buildType="aliBuild"
-    role="processing"
-    # Not necessarily meaningful when using aliBuild
-    softwarePath=""
-
-    # Additional settings
-    # None here, but more below (ie PYTHONPATH)
-elif [[ "$HOSTNAME" == "lbnl3core" ]];
-then
-    # Define necessary variables
-    projectPath="/home/rehlers/overwatch"
-    virtualEnvPath="/home/rehlers/overwatch/.env/bin/activate"
-    location="lbnl3"
-    buildType="alice-env"
-    role="processing"
-    softwarePath="/home/james/alice/"
-
-    # Additional settings
-    # None here, but more below (ie PYTHONPATH)
-else
-    echo "ERROR: Cannot run on hostname $HOSTNAME. You need to define the necessary variables."
-    safeExit 1
-    return $?
-fi
-
-echo "INFO: Running at $location"
+echoInfoEscaped "Running at $location"
 
 # Source virtualenv
-echo "INFO: Loading virtualenv"
+echoInfoEscaped "Loading virtualenv"
 source "$virtualEnvPath"
 
 # Setup ROOT
-echo "INFO: Loading ROOT"
+echoInfoEscaped "Loading ROOT"
 if [[ "$buildType" == "root" ]];
 then
     # Setup ROOT using thisroot.sh
@@ -134,7 +73,7 @@ then
 
     #if [[ "$sourcedScript" == true ]];
     #then
-        alienv load AliRoot/latest-aliMaster
+        alienv load "AliRoot/$alibuildName"
     #else
     #    eval "$(alienv load AliRoot/latest-aliMaster)"
     #fi
@@ -149,19 +88,22 @@ then
     # Load the alice environment
     source "$softwarePath"/alice-env.sh -n 1 -q
 else
-    echo "ERROR: Unrecognized build type $buildType! Exiting"
+    echoErrorEscaped "Unrecognized build type $buildType! Exiting"
     safeExit 1
     return $?
 fi
 
 if [[ "$sourcedScript" == true ]];
 then
-    echo "INFO: Environment ready for use!"
+    echoInfoEscaped "Environment ready for use!"
 else
     if [[ "$role" == "server" ]];
     then
+        #############################
         # Start web server
-        echo "INFO: Starting uwsgi with config at $projectPath/config/wsgi${location}.ini"
+        #############################
+
+        echoInfoEscaped "Starting uwsgi with config at $projectPath/config/wsgi${location}.ini"
         if [[ "$calledFromSystemd" == true ]];
         then
             uwsgi "$projectPath/config/wsgi${location}.ini" &> "$projectPath/app.log"
@@ -170,11 +112,150 @@ else
         fi
     elif [[ "$role" == "processing" ]];
     then
+        # In this case, we will start the receivers (if necessary), and execute processing
+
+        #############################
+        # Check whether the ZMQ receivers are running, and if not, start them
+        #############################
+        echo #EMPTY
+        echoInfoEscaped "Checking whether receivers exist, and starting them if not..."
+
+        # get the zmq processing running, and create an array of relevant info (process name, subsystem, port, PID)
+        arr=($(ps aux | awk '/[z]mqReceive/ {printf "%s %s %s %s\n", $11, $NF, $12, $2}'))
+
+        if [[ ${forceRestart} == true ]]; then
+          echoInfoEscaped "Force restart requested -- all previous receivers will be killed, and a new receiver will be started for each subsystem."
+        fi
+
+        if [[ $((${#arr[@]}/4)) -gt "${#subsystems[@]}" ]]; then
+          echoWarnEscaped "There are more zmqReceivers running than there are subsystems! Forcing a re-start of all receivers at relevant ports..."
+          forceRestart=true
+        fi
+        echo #EMPTY
+
+        # Loop through subsystems, and re-start each receiver if appropriate
+        for (( n=0; n<"${#subsystems[@]}"; n++ ))
+        do
+          subsystemLog="${currentLocation}/${subsystems[n]}Receiver.log"
+          echoInfoEscaped "See ${currentLocation}/${subsystems[n]}Receiver.log for details."
+          echoInfoEscaped "Checking for ${subsystems[n]} receiver..." > ${subsystemLog}
+
+          foundReceiver=false
+          startReceiver=true
+
+          # loop through running processes (each process stores the 4 entries above)
+          for (( i=0; i<${#arr[@]}; i=i+4 ))
+          do
+            processName_i=${arr[i]}
+            subsystem_i=${arr[i+1]}
+            port_i=${arr[i+2]}
+            pid_i=${arr[i+3]}
+
+            # check if subsystem and port and name match the process
+            if [[ $(echo $subsystem_i | grep "${subsystems[n]}") ]]; then
+              if [[ $(echo $port_i | grep "${internalReceiverPorts[n]}") ]]; then
+                if [[ $(echo ${processName_i} | grep "zmqReceive") ]]; then
+                  # We found a match
+                  echoInfoEscaped "Found ${subsystems[n]} receiver!" >> ${subsystemLog}
+                  foundReceiver=true
+                  startReceiver=false
+
+                  # If forceRestart is enabled, kill the receiver
+                  if [[ ${forceRestart} == true ]]; then
+                    kill ${pid_i}
+                    echoInfoEscaped "Force restart is requested -- killed receiver ${subsystems[n]}" >> ${subsystemLog}
+                    startReceiver=true
+                  fi
+
+                fi
+              fi
+            fi
+
+          done
+
+          if [[ ${foundReceiver} == false ]]; then
+            echoInfoEscaped "No ${subsystems[n]} receiver found!" >> ${subsystemLog}
+          fi
+
+          # If we don't find a match, or if the receiver has been killed, we must start a receiver for this subsystem
+          if [[ ${startReceiver} == true ]]; then
+
+            echoInfoEscaped "Starting receiver for ${subsystems[n]}..." >> ${subsystemLog}
+
+            # Move to data directory
+            dataLocation="${projectPath}/data"
+            echoInfoEscaped "Moving to data directory: \"${dataLocation}\"" >> ${subsystemLog}
+            cd "${dataLocation}"
+
+            if [[ "${subsystems[n]}" == "HLT" ]];
+            then
+              monitorPort=25006
+            else
+              monitorPort=25005
+            fi
+
+            additionalOptions=""
+
+            echoInfoEscaped "Receiver Settings:" >> ${subsystemLog}
+            echoPropertiesEscaped "Subsystem: ${subsystems[n]}" >> ${subsystemLog}
+            echoPropertiesEscaped "Receiver (interal) Port: ${internalReceiverPorts[n]}" >> ${subsystemLog}
+            echoPropertiesEscaped "Use SSH tunnel: $useSSHTunnel" >> ${subsystemLog}
+            echoPropertiesEscaped "Tunnel (external) Port: ${externalReceiverPorts[n]}" >> ${subsystemLog}
+            echoPropertiesEscaped "SSH Monitor Port: $monitorPort" >> ${subsystemLog}
+            echoPropertiesEscaped "Additional Options: $additionalOptions" >> ${subsystemLog}
+
+            # TO DO: update this to connect to HLT
+            if [[ "${useSSHTunnel}" == true ]];
+            then
+              # Find SSH process
+              sshProcesses=$(pgrep -f "autossh -M $monitorPort")
+              echoInfoEscaped "autossh PID: $sshProcesses"
+
+              # Determine if ssh tunnel is needed.
+              # autossh should ensure that the connection never dies.
+              if [[ -z "$sshProcesses" ]];
+              then
+                echoInfoEscaped "Did not find necessary ${subsystems[n]} autossh tunnel. Starting a new one!"
+                autossh -M $monitorPort -f -N -L ${internalReceiverPorts[n]}:localhost:${externalReceiverPorts[n]} emcalguest@lbnl5core.cern.ch
+              else
+                echoInfoEscaped "${subsystems[n]} autossh tunnel already found with PID $sshProcesses. Not starting another one."
+              fi
+            else
+              echoInfoEscaped "Not using a SSH tunnel!" >> ${subsystemLog}
+            fi
+
+            # select="" ensures that we get all histograms(?)
+            receiverPath="${projectPath}/receiver/bin"
+            nohup "${receiverPath}/zmqReceive" --in="REQ>tcp://localhost:${internalReceiverPorts[n]}" --verbose=1 --sleep=60 --timeout=100 --select="" --subsystem="${subsystems[n]}" ${additionalOptions} >> ${subsystemLog} 2>&1 &
+
+            # Ensure that the output is still readable and that the receiver is able to start.
+            sleep 1
+
+            # Moving back to initOVERWATCH directory
+            echoInfoEscaped "Moving back to deploy directory: \"$currentLocation\"" >> ${subsystemLog}
+            cd "$currentLocation"
+
+            echoInfoEscaped "Finished starting ${subsystems[n]} receiver!" >> ${subsystemLog}
+
+          else
+            echoInfoEscaped "No need to start new ${subsystems[n]} receiver." >> ${subsystemLog}
+          fi
+
+        done
+
+        echo #EMPTY
+        echoInfoEscaped "Done configuring receivers! All receivers are now running."
+        echo #EMPTY
+
+        #############################
+        # Execute processing
+        #############################
+
         if [[ "$calledFromSystemd" == true ]];
         then
-            echo "ERROR: Processing should not be started from $1! Exiting"
-            safeExit 1
-            return $?
+          echoErrorEscaped "Processing should not be started from $1! Exiting"
+          safeExit 1
+          return $?
         fi
 
         # Check for lockout file
@@ -184,21 +265,22 @@ else
 
         # Only process if we are not locked out.
         # Locking out allows changes to processing (for example, reprocessing) without having to disable the crontab
-        echo "INFO: Checking for lockOut file."
+        echoInfoEscaped "Checking for lockOut file."
         if [[ -e "${currentLocation}/lockOut" ]];
         then
-            echo "WARNING: Processing locked out! Remove \"lockOut\" file to resume processing with this script! Exiting"
+            echoWarnEscaped "Processing locked out! Remove \"lockOut\" file to resume processing with this script! Exiting"
             safeExit 1
             return $?
         fi
 
         cd "$projectPath"
         # -b for batch mode (ie no graphics)
-        echo "INFO: Starting processing"
+        echoInfoEscaped "Starting processing"
         python processRuns.py -b
     else
-        echo "ERROR: Role $role not recognized. Exiting"
+        echoErrorEscaped "Role $role not recognized. Exiting"
         safeExit 1
         return $?
     fi
+
 fi
