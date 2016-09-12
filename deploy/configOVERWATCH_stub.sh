d178665b36fed295a99e40e32b458cac03519750
Raymond Ehlers <raymond.ehlers+git@gmail.com>
2016-09-12 17:31:57 -0400

    Make configuration into proper stub
    And ignore the standard configuration file

--- deploy/configOVERWATCH_stub.sh
+++ deploy/configOVERWATCH_stub.sh
@@ -32,44 +32,7 @@ buildType=""
 alibuildName="latest-aliMaster"
 role=""
 
-if [[ $HOSTNAME == *"pdsf"* || $HOSTNAME == *"sgn"* ]];
-then
-    # Define necessary variables
-    projectPath="/project/projectdirs/alice/aliprodweb/overwatch"
-    softwarePath="/project/projectdirs/alice/aliprodweb/ROOT/"
-    virtualEnvPath="/project/projectdirs/alice/aliprodweb/virtualenv/python_2_7_11/bin/activate"
-    location="PDSF"
-    buildType="root"
-    role="server"
-
-    # Additional settings
-    # Sets the library path for both python and libffi
-    export LD_LIBRARY_PATH="/project/projectdirs/alice/aliprodweb/python_bin/v2.7.11/lib:/project/projectdirs/alice/aliprodweb/essentials/install/lib64:$LD_LIBRARY_PATH"
-elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"yale"* ]];
-then
-    # Define necessary variables
-    projectPath="/opt/www/aliceoverwatch"
-    softwarePath="/opt/aliceSW/root/alice_v5-34-30/inst/"
-    virtualEnvPath="/opt/www/aliceoverwatch/.env/bin/activate"
-    location="Yale"
-    buildType="root"
-    role="server"
-
-    # Additional settings
-    # None!
-elif [[ $(hostname -f) == *"aliceoverwatch"* && $(hostname -f) == *"cern"* ]];
-then
-    # Define necessary variables
-    projectPath="/home/emcal/overwatch"
-    softwarePath="/home/emcal/alice/sw"
-    virtualEnvPath="/home/emcal/overwatch/.env/bin/activate"
-    location="overwatchCERN"
-    buildType="aliBuild"
-    role="processing"
-
-    # Additional settings
-    # None!
-elif [[ "${docker}" == true ]];
+if [[ "${docker}" == true ]];
 then
     projectPath="/overwatch"
     softwarePath="/alice/sw"
@@ -80,16 +43,8 @@ then
 
     # Additional settings
     # None!
-elif [[ $(hostname -f) == *"ray-MBP"* ]];
-then
-    # TEMP!
-    projectPath="$HOME/code/alice/overwatch"
-    softwarePath="$HOME/alice/sw"
-    virtualEnvPath="$projectPath/.env/bin/activate"
-    location="ray-MBP"
-    buildType="aliBuild"
-    role="processing"
 
+# Add additional systems here!
 else
     echoErrorEscaped "Cannot run on hostname $HOSTNAME. You need to define the necessary variables."
     safeExit 1
