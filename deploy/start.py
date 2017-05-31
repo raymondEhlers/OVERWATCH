d8a81e28f07f9295f67507e72aefd70dc1af4b28
Raymond Ehlers <raymond.ehlers@gmail.com>
2017-05-31 11:36:41 -0400

    Added proper logging

--- deploy/start.py
+++ deploy/start.py
@@ -2,11 +2,13 @@
 
 from __future__ import print_function
 
+import logging
 import argparse
 import subprocess
 import sys
-
 import yaml
+
+logger = logging.getLogger("")
 
 # Convenience
 import pprint
@@ -23,17 +25,17 @@ def checkForProcessPID(processIdentifier):
         res = subprocess.check_output(["pgrep", "-f", "{0}".format(processIdentifier)])
     except subprocess.CalledProcessError as e:
         if e.returncode == 1:
-            print("Process identifier \"{0}\" not found! It will be created.".format(processIdentifier))
+            logger.info("Process identifier \"{0}\" not found! It will be created.".format(processIdentifier))
             return None
         else:
-            print("Unknown return code of \"{0}\" for command \"{1}\", with output \"{2}".format(e.returncode, e.cmd, e.output))
+            logger.critical("Unknown return code of \"{0}\" for command \"{1}\", with output \"{2}".format(e.returncode, e.cmd, e.output))
             sys.exit(e.returncode)
     # Convert from bytes to string when returning
     # Each entry is on a new line (and we must check for empty lines)
     pids = res.decode("utf-8").split("\n")
     pids = [int(pid) for pid in pids if pid != ""]
     if len(pids) > 1:
-        print("WARNING: Multiple PIDs associated with this receiver!")
+        logger.warning("Multiple PIDs associated with this receiver!")
     return pids
     # sshProcesses=$(pgrep -f "autossh -L ${internalReceiverPorts[n]}")
 
@@ -48,15 +50,15 @@ def tunnel(config):
 def receiver(config):
     """ Start receivers """
     for receiver, receiverConfig in config["receiver"].items():
-        print("receiver name: {0}, config: {1}".format(receiver, receiverConfig))
+        logger.debug("receiver name: {0}, config: {1}".format(receiver, receiverConfig))
         if len(receiver) != 3:
-            print("Skipping entry \"{0}\" in the receiver config, as it it doesn't correspond to a detector".format(receiver))
+            logger.debug("Skipping entry \"{0}\" in the receiver config, as it it doesn't correspond to a detector".format(receiver))
             continue
         # Ensure that the detector name is upper case
         receiver = receiver.upper()
 
         processExists = checkForProcessPID("/Users/re239/code/alice/overwatch/receiver/bin/zmqReceive --subsystem={0}".format(receiver))
-        print("processExists: {0}".format(processExists))
+        logger.debug("processExists: {0}".format(processExists))
 
         # Check whether we should kill the receivers
 
@@ -73,8 +75,8 @@ def receiver(config):
                     ]
             if "additionalOptions" in config:
                 args = args + config["additionalOptions"]
-            with open("{0}ReceiverTest.log".format(receiver), "wb") as logFile:
-                print("Starting receiver with args: {0}".format(args))
+            with open("{0}Receiver.log".format(receiver), "wb") as logFile:
+                logger.info("Starting receiver with args: {0}".format(args))
                 process = subprocess.Popen(args, stdout=logFile, stderr=subprocess.STDOUT)
                 #--verbose=1 --sleep=60 --timeout=100 --select="" --subsystem="${subsystems[n]}" ${additionalOptions}
 
@@ -85,7 +87,7 @@ def receiver(config):
                 #
                 # --> Don't need to escape most quotes!
         else:
-            print("Skipping receiver \"{0}\" since it already exists!".format(receiver))
+            logger.info("Skipping receiver \"{0}\" since it already exists!".format(receiver))
 
 def processing(config):
     """ Start processing. """
@@ -109,7 +111,7 @@ def startOverwatch(filename):
     with open(filename, "rb") as f:
         config = yaml.load(f)
 
-    print("Config:")
+    logger.info("Config:")
     pprint.pprint(config)
 
     if "receiver" in config and config["receiver"]["enabled"]:
@@ -134,8 +136,21 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--config", metavar="configFile",
                         type=str, default="deployConfig.yaml",
                         help="Path to config filename")
+    parser.add_argument("-l", "--logLevel", metavar="level",
+                        type=str, default="DEBUG",
+                        help="Set the log leve.")
 
     # Parse arguments
     args = parser.parse_args()
+
+    # Setup logger
+    formatter = logging.Formatter("[%(levelname)s] %(message)s")
+    streamHandler = logging.StreamHandler(sys.stdout)
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+
+    # Set log level
+    level = args.logLevel.upper()
+    logger.setLevel(level)
 
     startOverwatch(args.config)
