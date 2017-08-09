7df0885f1527b1a443f061cd56644dc55da52e0f
Raymond Ehlers <raymond.ehlers@gmail.com>
2017-08-09 15:46:03 -0400

    Cleanup

--- deploy/docker/setupServerConfig.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Setup serverParams.py
-sed -e 's/defaultUsername = ""/defaultUsername = "user"/g' ../../config/serverParams.py > serverParams.py
--- deploy/docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo $PWD
-
-# Setup
-python /opt/overwatch/deploy/start.py -e config
-# Run supervisord to handle signals
-/usr/bin/supervisord -c ../supervisord.conf
