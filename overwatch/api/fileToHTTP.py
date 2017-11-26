9c82088d835bd8df26877d1afdd498783a00b9d1
Raymond Ehlers <raymond.ehlers@gmail.com>
2017-11-26 23:09:30 +0100

    Cleanup file access via HTTP

--- overwatch/api/fileToHTTP.py
+++ overwatch/api/fileToHTTP.py
@@ -9,11 +9,13 @@ import tempfile
 
 host = "http://127.0.0.1:5000/rest/api/v1/files"
 
-def getFile(filename, fileObject):
+def getFile(filename, fileObject, stream = False):
     fileInMemory = StringIO.StringIO()
     r = requests.get("{host}/{filename}".format(host = host, filename = filename), stream=True)
     print("response: {}".format(r))
     if r.ok:
+        # Write in chunks to allow for streaming. See: https://stackoverflow.com/a/13137873
+        # To stream a response, we need a generator. See: https://gist.github.com/gear11/8006132#file-main-py-L36
         for chunk in r:
             fileObject.write(chunk)
         # Return to start of file so the read is seamless
@@ -22,13 +24,35 @@ def getFile(filename, fileObject):
     
     return (r.ok, r.status_code, fileObject)
 
+def putFile(filename, file = None, localFilename = None):
+    """ Use StringIO to write from memory. """
+    if not file and not filename:
+        print("Please pass a valid file or filename")
+
+    if filename and not file:
+        file = open(filename, "rb")
+
+    r = requests.put("{host}/{filename}".format(host = host, filename = filename), files = { "file": file })
+    return (r.ok, r.status_code, r.text)
+
 @contextlib.contextmanager
 def FileInMemory(filename, writeFile = False):
     try:
         fileInMemory = StringIO.StringIO()
         yield getFile(filename = filename, fileObject = fileInMemory)
+        print("Successfully completed FileInMemory")
+    except IOError as e:
+        # Just need an exception so that else is valid.
+        print("IOError: {}".format(e))
+    else:
+        # Only do this if there are no exceptions above
+        print("Potentially writing file")
+        if writeFile:
+            (success, status, returnValue) = putFile(filename = filename, file = fileInMemory)
+            print("Successfully wrote file")
     finally:
         fileInMemory.close()
+        print("Finally exiting from FileInMemory")
 
 # See: https://stackoverflow.com/a/28401296
 @contextlib.contextmanager
@@ -50,12 +74,9 @@ def FileWithLocalFilename(filename, writeFile = False):
                     #print("f.read(): {}".format(f.read()))
 
                     yield f.name
-
                     #print("Post yield")
                     #f.seek(0, os.SEEK_END)
                     #print("f length in with def: {}".format(f.tell()))
-
-                    fileInMemory.close()
                 else:
                     #yield (False, status, fileInMemory)
                     yield False
@@ -71,32 +92,6 @@ def FileWithLocalFilename(filename, writeFile = False):
                 print("Successfully wrote file")
         finally:
             print("Finally exiting from FileWithLocalFilename")
-
-def GetFileWithLocalFilename(object):
-    def __init__(self, filename):
-        self._filename = filename
-
-    def __enter__(self):
-        (success, status, fileInMemory) = getFile(self._filename)
-
-        if success:
-            f.write(fileInMemory.read())
-            return f.name
-
-    def __exit__(self, *args, **kwargs):
-        self._temporaryFile.close()
-
-
-def putFile(filename, file = None, localFilename = None):
-    """ Use StringIO to write from memory. """
-    if not file and not filename:
-        print("Please pass a valid file or filename")
-
-    if filename and not file:
-        file = open(filename, "rb")
-
-    r = requests.put("{host}/{filename}".format(host = host, filename = filename), files = { "file": file })
-    return (r.ok, r.status_code, r.text)
 
 if __name__ == "__main__":
     # Get the file
