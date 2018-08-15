66700f37ba2b761ac32cbc9740d7895d72b84870
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-08-15 17:32:06 -0400

    Add back __init__.py for detector module
    Such a file is necessary for python 2. It was unintentionally removed
    when removing obsolete documentation.

---
+++ overwatch/processing/detectors/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" Contains subsystem specific functionality implemented through the plugin system.
+
+For information on writing plugins, see the ``README.md`` file in this directory.
+
+.. codeauthor:: Raymond Ehlers <raymond.ehlers@cern.ch>, Yale University
+"""
+
+# NOTE: `__all__` is intentionally not specified here. The subsystems are loaded dynamically
+#       based on a configuration set elsewhere, so we don't want to set any misleading options here.
