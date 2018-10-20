0458c44b43531160b02ffc7b785edbc706b24093
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-10-20 00:47:51 -0400

    Fix data transfer sentry environment variable
    Forgot one "dataHandling" -> "dataTransfer"

--- overwatch/base/run.py
+++ overwatch/base/run.py
@@ -36,7 +36,7 @@ logger.info(parameters)
 # Setup sentry to create alerts for warning level messages.
 sentry_logging = LoggingIntegration(level = logging.WARNING, event_level = None)
 # Usually, we want the module specific DSN, but we will take the general one if it's the only one available.
-sentry_sdk.init(dsn = os.getenv("SENTRY_DSN_DATA_HANDLING") or os.getenv("SENTRY_DSN"), integrations = [sentry_logging])
+sentry_sdk.init(dsn = os.getenv("SENTRY_DSN_DATA_TRANSFER") or os.getenv("SENTRY_DSN"), integrations = [sentry_logging])
 
 # Imports are below here so that they can be logged
 from overwatch.base import dataTransfer
