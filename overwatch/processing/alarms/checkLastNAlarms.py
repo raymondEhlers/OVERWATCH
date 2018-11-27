174e2036ed558da325ee2c6abcae3ccd6720fd19
Nabywaniec <Nabywaniec@users.noreply.github.com>
2018-11-27 18:30:00 +0100

    Update checkLastNAlarms.py

--- overwatch/processing/alarms/checkLastNAlarms.py
+++ overwatch/processing/alarms/checkLastNAlarms.py
@@ -1,23 +1,23 @@
 from .alarm import Alarm
 
 
-class IncreasingValueAlarm(Alarm):
-    def __init__(self, minVal=0, maxVal=100, *args, **kwargs):
+class checkLastNAlarms(Alarm):
+    def __init__(self, minVal=0, maxVal=100, ratio=0.6, N=5, *args, **kwargs):
         super(IncreasingValueAlarm, self).__init__(*args, **kwargs)
         self.minVal = minVal
         self.maxVal = maxVal
-        self.ratio = 0.6
-        self.N = 5
+        self.ratio = ratio
+        self.N = N
 
     def checkAlarm(self, trend):
-        trendingValues = trend[-self.N:]
-        if len(trendingValues) < self.N:
+        if len(trend.trendedValues) < self.N:
             return False
-        notInBorderValues = [trendValue for trendValue in trendingValues if self.maxVal > trendingValues > self.minVal]
-        if len(notInBorderValues) > (1 - self.ratio) * self.N:
+        trendedValues = trend[-self.N:]
+        inBorderValues = [trendedValue for trendedValue in trendedValues if self.maxVal > trendedValue > self.minVal]
+        if len(inBorderValues) >= self.ratio * self.N:
             return False
 
-        alarm = "more than {} % values of last: {} values not in {} {}".format(self.ratio * 10, self.N, self.minVal,
+        alarm = "less than {} % values of last: {} values not in {} {}".format(self.ratio * 10, self.N, self.minVal,
                                                                                self.maxVal)
         self._announceAlarm(alarm)
         return True
