7cd7c89e88cc3b276ae76d08d41b82f8c79cc966
Nabywaniec <Nabywaniec@users.noreply.github.com>
2018-11-27 18:31:34 +0100

    Update and rename checkLastNAlarms.py to checkLastNAlarm.py

--- overwatch/processing/alarms/checkLastNAlarm.py
+++ overwatch/processing/alarms/checkLastNAlarm.py
@@ -1,9 +1,9 @@
 from .alarm import Alarm
 
 
-class checkLastNAlarms(Alarm):
+class checkLastNAlarm(Alarm):
     def __init__(self, minVal=0, maxVal=100, ratio=0.6, N=5, *args, **kwargs):
-        super(IncreasingValueAlarm, self).__init__(*args, **kwargs)
+        super(checkLastNAlarm, self).__init__(*args, **kwargs)
         self.minVal = minVal
         self.maxVal = maxVal
         self.ratio = ratio
