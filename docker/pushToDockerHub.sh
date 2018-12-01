ff4c6f9bff0b64255257c139d74653a6c981e3a8
Raymond Ehlers <raymond.ehlers@gmail.com>
2018-12-01 14:58:35 -0500

    Fix docker push during Travis tag
    It deployed to docker based on the branch name regardless of whether the
    commit was tagged, but we didn't look for branch names in the form of
    tags. So this modifies and documents the branches which should deploy
    to docker, and the condition is satisfied for tags. Thus, now docker
    images are deployed on tagging

--- docker/pushToDockerHub.sh
+++ docker/pushToDockerHub.sh
@@ -2,6 +2,6 @@
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
-imageName="${DOCKER_USERNAME}/overwatch:${TRAVIS_TAG:=latest}-py${DOCKER_PYTHON_VERSION}"
+imageName="${DOCKER_USERNAME}/overwatch:${TRAVIS_TAG:-latest}-py${DOCKER_PYTHON_VERSION}"
 echo "Pushing image ${imageName} to Docker Hub"
 docker push "${imageName}"
